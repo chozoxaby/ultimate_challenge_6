3d83a5e22c1178f5fca6702f67b9ad2dafa43755
chozoxaby@gmail.com <jcwllms@gmail.com>
2017-11-11 16:41:22 +0000

    Part 3: Created a province model, configured it for CRUD in active admin, added a province_id migration, modified the customer and province models associations, modified the customer partial to display the provinces.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
+  belongs_to :province
+
   validates :full_name, :phone_number, presence: true
 	validates :phone_number, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Province.create(name: 'British Columbia')
+Province.create(name: 'Manitoba')
+Province.create(name: 'New Brunswick')
+Province.create(name: 'Newfoundland and Labrador')
+Province.create(name: 'Nova Scotia')
+Province.create(name: 'Ontario')
+Province.create(name: 'Prince Edward Island')
+Province.create(name: 'Quebec')
+Province.create(name: 'Saskatchewan')
